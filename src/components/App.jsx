50cdf09bf4e8a7cca95ae483edeafd91a9436eaa
DariaZharkova <139045140+DariaZharkova@users.noreply.github.com>
2024-01-13 21:53:35 +0200

    add friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Profile } from './Profile';
+import { FriendList } from './FriendList';
 import userData from '../userData.json';
+import friends from '../friends.json';
 
 export const App = () => {
   return (
@@ -11,6 +13,7 @@ export const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
     </>
   );
 };
