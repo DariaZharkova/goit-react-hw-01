a4be866bfbf3cccb7a4676d0d7a2625ed5a91e58
DariaZharkova <139045140+DariaZharkova@users.noreply.github.com>
2024-01-14 15:49:56 +0200

    add profile styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import { Profile } from './Profile';
-import { FriendList } from './FriendList';
+import { Profile } from './Profile/Profile';
+import { FriendList } from './FriendList/FriendList';
 import userData from '../userData.json';
 import friends from '../friends.json';
 
